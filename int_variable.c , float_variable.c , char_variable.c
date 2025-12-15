a3d5e3b2c67256b118f9b53d6c0f41505171bc14
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-15 08:22:44 +0530

    Add files for int, float, and char variable examples

---
+++ int_variable.c , float_variable.c , char_variable.c
@@ -0,0 +1,29 @@
+int_variable.c
+
+  #include <stdio.h>
+
+int main() {
+    int a = 10;
+    printf("Value of a = %d", a);
+    return 0;
+}
+
+float_variable.c
+
+  #include <stdio.h>
+
+int main() {
+    float pi = 3.14;
+    printf("Value of pi = %.2f", pi);
+    return 0;
+}
+
+char_variable.c
+
+  #include <stdio.h>
+
+int main() {
+    char ch = 'A';
+    printf("Character = %c", ch);
+    return 0;
+}
